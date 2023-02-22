88b46d35b2845a882e6267cf81a4554671b9b417
praveenreddiey <praveenreddiey@gmail.com>
2023-02-22 23:26:29 +0530

    day5 of learningJS

---
+++ 02-Fundamentals-Part-2/starter/codingChallenge3.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const Mark = {
+  fullName: "Mark Miler",
+  mass: 92,
+  height: 1.95, //1.95
+  calcBMI: function () {
+    this.BMI = this.mass / (this.height * this.height);
+    return this.BMI;
+  },
+};
+
+const John = {
+  fullName: "John Smith",
+  mass: 78, //78
+  height: 1.69,
+  calcBMI: function () {
+    this.BMI = this.mass / (this.height * this.height);
+    return this.BMI;
+  },
+};
+Mark.calcBMI();
+John.calcBMI();
+console.log(Mark.BMI);
+console.log(John.BMI);
+
+//or we can use with if condition
+
+const higherBMI =
+  Mark.BMI > John.BMI
+    ? console.log(
+        `${Mark.fullName}'s BMI (${Mark.BMI}) is higher than ${John.fullName}'s BMI (${John.BMI}) `
+      )
+    : console.log(
+        `${John.fullName}'s BMI (${John.BMI}) is higher than ${Mark.fullName}'s BMI (${Mark.BMI}) `
+      );
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -67,7 +67,68 @@ console.log(movies);
 const calcAge1 = (birthYear) => 2023 - birthYear;
 
 const years = [1997, 1996, 1995];
+//const years = new Array(1997,'praveen');
 for (let i = 0; i < years.length; i++) {
   const age = calcAge1(years[i]);
   console.log(`age is ${age}`);
 }
+
+//objects
+
+const person = {
+  p_name: "praveen",
+  //age: 2023 - 1997,
+  birthYear: 1997,
+  job: "software developer",
+  friends: ["pankaj", "prasanth", "chiru", "harsha"],
+  // age: function () {
+  //   return 2023 - this.birthYear;
+  // },
+  calcAge: function () {
+    this.age = 2023 - this.birthYear;
+    return this.age;
+  },
+  hasDriversLicense: true,
+};
+
+//dot and bracket notation
+
+console.log(person.friends[0]);
+console.log(person.p_name);
+console.log(person["p_name"]);
+
+const name = "name";
+console.log(person["p_" + name]);
+
+const interstedIn = prompt(
+  "What do you want to know about the person name. job, age, friends ?"
+);
+console.log(interstedIn);
+
+//console.log(person.interstedIn); it does not work as intersted in not a property of person object
+//undefined is a falsy value
+
+if (person[interstedIn]) console.log(person[interstedIn]);
+else console.log("wrong request! choose between name, age, job and friends");
+
+person.location = "hyderabad";
+person["relation"] = "single";
+console.log(person);
+
+//challenge
+console.log(
+  `${person.p_name} has ${person.friends.length} friends and his best friend is ${person.friends[0]}`
+);
+console.log(person.calcAge());
+console.log(person.age);
+console.log(person.age);
+console.log(person.age);
+console.log(person.age);
+
+//challenge
+
+const driversLicenseStatus = () => {
+  var status = this.hasDriversLicense ? "a" : "no";
+  return `${person.p_name} is a ${person.age} years old and he has ${status} drivers license`;
+};
+console.log(driversLicenseStatus());
