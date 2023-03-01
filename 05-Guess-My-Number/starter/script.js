6c5d067608be40d3e1a65606bf9922c745dd2510
praveenreddiey <praveenreddiey@gmail.com>
2023-03-01 22:58:10 +0530

    day9 of learningJS

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.score').textContent = 13;
+// document.querySelector('.number').textContent = 20;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', () => {
+  let guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else {
+    document.querySelector('.message').textContent = 'guessed';
+  }
+});
